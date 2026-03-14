5bf238850f74589224d6959b1fcd55cd8a441123
Антон <mikolenko.anton1@gmail.com>
2026-03-14 19:09:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -722,4 +722,15 @@ if (authToken) {
     localStorage.removeItem('k_login');
     localStorage.removeItem('kprofile');
   });
+
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (err) {
+      console.error('SW registration failed:', err);
+    }
+  });
+}
+
 }
